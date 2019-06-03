05e76831dce468c82bead630493cce70aeebf70e
Karen Chen <kchen6600@gmail.com>
2019-06-03 18:09:46 -0400

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Karen Chen on 6/3/19.
+//  Copyright © 2019 Karen Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
