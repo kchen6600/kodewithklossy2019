0f6b36ac28f70b73df2453829cc36a9279825931
Karen Chen <kchen6600@gmail.com>
2019-06-04 21:59:29 -0400

    complete iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -15,10 +15,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func cryingButtonTapped(_ sender: Any) {
+    @IBAction func cryingButtonTapped(_ sender: UIButton) {
     }
     
-    @IBAction func happyButtonTapped(_ sender: Any) {
+    @IBAction func happyButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func showMessage(sender: UIButton!) {
+        
+        let emotion = sender.currentTitle!
+        switch emotion {
+        case "😭":
+            // when user clicks the crying emoji button, show calming message
+            let alertController = UIAlertController(title: "We all get EMOJIonal", message: "Take a deep breath", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        case "😄":
+            // when user clicks the happy emoji button, show encouraging message
+            let alertController = UIAlertController(title: "We all get EMOJIonal", message: "YOU ROCK", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        default: break
+        }
+        
     }
 }
 
